01ddd62c0289a84644c102a4167fa5754405638b
LauraOviedo0810 <laurakoviedo@gmail.com>
2025-11-18 17:41:43 -0300

    Actualizo gitignore y vistas

--- mi_primera_pagina_Oviedo/app_principal/views.py
+++ mi_primera_pagina_Oviedo/app_principal/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Producto
 
 def lista_productos(request):
+    if request.method == "POST":
+        nombre = request.POST.get("nombre")
+        precio = request.POST.get("precio")
+        descripcion = request.POST.get("descripcion")
+
+        Producto.objects.create(
+            nombre=nombre,
+            precio=precio,
+            descripcion=descripcion
+        )
+        return redirect('lista_productos')
+
     productos = Producto.objects.all()
     return render(request, 'app_principal/lista_objetos.html', {'productos': productos})
