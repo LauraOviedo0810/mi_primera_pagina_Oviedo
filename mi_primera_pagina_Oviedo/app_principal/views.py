54201da35a776fd9d8f95593b878a4e6be15e94a
LauraOviedo0810 <laurakoviedo@gmail.com>
2025-11-19 13:37:27 -0300

    Actualizo templates y agrego formulario de productos

--- mi_primera_pagina_Oviedo/app_principal/views.py
+++ mi_primera_pagina_Oviedo/app_principal/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Producto
+from .forms import ProductoForm
 
 def lista_productos(request):
     if request.method == "POST":
-        nombre = request.POST.get("nombre")
-        precio = request.POST.get("precio")
-        descripcion = request.POST.get("descripcion")
-
-        Producto.objects.create(
-            nombre=nombre,
-            precio=precio,
-            descripcion=descripcion
-        )
-        return redirect('lista_productos')
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("lista_productos")  # evita re-enviar formularios
+    else:
+        form = ProductoForm()
 
     productos = Producto.objects.all()
-    return render(request, 'app_principal/lista_objetos.html', {'productos': productos})
+
+    return render(request, "app_principal/lista_objetos.html", {
+        "form": form,
+        "productos": productos
+    })
